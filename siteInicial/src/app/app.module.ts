be43515962bc2673ebef6c2bbbdc7a87ab2d3009
Mizrain Phelipe Sa <mizrain.sa@gmail.com>
2021-06-12 00:22:47 -0300

    Novas configurações base
    Config base do projeto

--- siteInicial/src/app/app.module.ts
+++ siteInicial/src/app/app.module.ts
@@ -1,19 +1,27 @@
+/* ARQUIVO PRINCIPAL dos Imports da App */
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Novos imports a partir daqui
+
 @NgModule({
+  // Os componentes são declarados aqui
   declarations: [
     AppComponent
   ],
+
+  // Os módulos são importados aqui
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
+  
+  // Aqui são os services etc
   providers: [],
   bootstrap: [AppComponent]
 })
