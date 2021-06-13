44e0e33bc9dcab6eaa057f493f771d85e8c1f030
Mizrain Phelipe Sa <mizrain.sa@gmail.com>
2021-06-13 00:50:15 -0300

    commit novas alterações
    configurando base do projeto e implementando componentes principais

--- siteInicial/src/app/app-routing.module.ts
+++ siteInicial/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Imports nossos
+import { HomeComponent } from './components/views/pages/home/home.component';
+
+const routes: Routes = [
+   { path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- siteInicial/src/app/app.module.ts
+++ siteInicial/src/app/app.module.ts
@@ -5,20 +5,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 //Novos imports a partir daqui
+import { HomeComponent } from './components/views/pages/home/home.component';
+import { HeaderComponent } from './components/views/template/header/header.component';
+import { FooterComponent } from './components/views/template/footer/footer.component';
+import { NavComponent } from './components/views/template/nav/nav.component';
+import { SidebarComponent } from './components/views/template/sidebar/sidebarR.component';
+import { Sidebar2Component } from './components/views/template/sidebar/sidebar2.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   // Os componentes são declarados aqui
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    SidebarComponent,
+    Sidebar2Component,
   ],
 
   // Os módulos são importados aqui
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   
   // Aqui são os services etc
